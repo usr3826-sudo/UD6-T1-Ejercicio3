3add606dd12cbb78b910b2bd8433d2593c5f7831
usr3826-sudo <usr3826@salesianos-lacuesta.net>
2026-04-30 22:32:02 +0200

    featClase videojuegos, sus atributos y su constructor

---
+++ src/net/salesianos/Videojuegos/Videojuegos.java
@@ -0,0 +1,14 @@
+package net.salesianos.Videojuegos;
+
+public class Videojuegos {
+    private String titulo;
+    private String genero;
+    private int horasJugadas;
+
+    public Videojuegos(String titulo, String genero, int horasJugadas) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.horasJugadas = horasJugadas;
+    }
+      
+}
